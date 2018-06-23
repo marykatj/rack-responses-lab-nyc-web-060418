73d804ab3bfe32e5b0a1bd69c1d4eff6448cd699
Mary-Kate James <marykatj@gmail.com>
2018-06-23 18:43:05 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,15 @@
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+
+    if Time.now > 12:00:00
+      resp.write "Good Morning!"
+    else
+      resp.write "Good Afternoon!"
+    end
+  resp.finish
+  end
+
+
+end
